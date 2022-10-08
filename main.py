1dc6552b25729454c827a62b226a742038624cbf
0sad4y <ilyaosadchiy02@gmail.com>
2022-10-08 03:06:11 +0300

    create 2, 3 methods

--- main.py
+++ main.py
@@ -1,9 +1,54 @@
 import functions
 import matrix
+from minimum import find_minimum
+import alpha
 
 
 def main():
-    pass
+    function_index = 1
+    method_index = 3
+    accuracy = 0.001
+    x = [0, 0]
+    h = [0, 0]
+    grad = []
+    f = functions.Functions(function_index)
+
+    if method_index == 1:
+        pass
+    elif method_index == 2:
+        while True:
+            grad = matrix.gradient(f, x)
+
+            if matrix.norm(grad) < accuracy:
+                break
+
+            a = alpha.Alpha(f, x, grad, False)
+            a_min = find_minimum(a)
+
+            for i in range(len(x)):
+                x[i] -= a_min * grad[i]
+
+        result = x
+    elif method_index == 3:
+        while True:
+            grad = matrix.gradient(f, x)
+
+            if matrix.norm(grad) < accuracy:
+                break
+
+            h = matrix.gesse_matrix(f, x)
+            h = matrix.inverse_matrix(h)
+            p = matrix.mul(matrix.mul(h, -1.0), grad)
+
+            a = alpha.Alpha(f, x, p, True)
+            a_min = find_minimum(a)
+
+            for i in range(len(x)):
+                x[i] += a_min * p[i]
+
+        result = x
+
+    print(result)
 
 
 if __name__ == '__main__':
