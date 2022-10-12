e547633789cf12e4e1d11f49d77722f7bfa4d9e7
0sad4y <ilyaosadchiy02@gmail.com>
2022-10-13 00:45:05 +0300

    create 2.1.2

--- main.py
+++ main.py
@@ -5,16 +5,59 @@ import alpha
 
 
 def main():
-    function_index = 1
+    function_index = 4
     method_index = 3
     accuracy = 0.001
-    x = [0, 0]
-    h = [1, 1]
-    grad = []
+    z = 0.1
+    result = 0
+    x = [3, -1, 0, 1]
+    h = [1, 1, 1, 1]
     f = functions.Functions(function_index)
 
     if method_index == 1:
-        pass
+        def search(b: list):
+            temp_b = b.copy()
+            tb = f.func(temp_b)
+            temp_h = [0] * len(temp_b)
+
+            for itr in range(len(temp_b)):
+                temp_he = temp_h.copy()
+                temp_he[itr] = h[itr]
+                t = f.func(matrix.add(temp_b, temp_he))
+
+                if t < tb:
+                    temp_b = matrix.add(temp_b, temp_he)
+                    tb = t
+                else:
+                    temp_he = temp_h.copy()
+                    temp_he[itr] = h[itr]
+                    t = f.func(matrix.sub(temp_b, temp_he))
+
+                    if t < tb:
+                        temp_b = matrix.sub(temp_b, temp_he)
+                        tb = t
+            return temp_b
+
+        b1 = x.copy()
+
+        while True:
+            yk = b1.copy()
+            b2 = search(yk)
+
+            while True:
+                yk = matrix.add(b1, matrix.mul(matrix.sub(b2, b1), 2))
+                y = search(yk)
+                b1 = b2.copy()
+                if f.func(y) >= f.func(b1):
+                    break
+                b2 = y.copy()
+
+            if f.func(y) == f.func(b1):
+                if matrix.norm(h) <= accuracy:
+                    break
+                h = matrix.mul(h, z)
+
+        result = b1
     elif method_index == 2:
         while True:
             grad = matrix.gradient(f, x)
@@ -49,6 +92,7 @@ def main():
         result = x
 
     print(result)
+    print(f.func(result))
 
 
 if __name__ == '__main__':
